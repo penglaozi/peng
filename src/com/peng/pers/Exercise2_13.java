0fc1b54587e2cba915939ed2049e8baf493f8e70
penglaozi <704365628@qq.com>
2016-08-09 23:34:38 +0800

    java dialog

--- src/com/peng/pers/Exercise2_13.java
+++ src/com/peng/pers/Exercise2_13.java
@@ -1,6 +1,6 @@
 package com.peng.pers;
 
-//import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
 
 public class Exercise2_13 {
 	public static void main(String[] args){
@@ -12,7 +12,9 @@ public class Exercise2_13 {
 		
 		int numberOfOneDollars = remainingAmount / 100;
 		remainingAmount %= 100; */
-		//String input = JOptionPane.showInputDialog("");
+		String input = JOptionPane.showInputDialog("Enter an amount in double, for example 11.56: ");
+		double amount = Double.parseDouble(input);
+		System.out.println(amount);
 	}
 
 }
