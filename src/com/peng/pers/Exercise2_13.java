78e6fb7b711286f7403810ac23b435ef6e42dffd
penglaozi <704365628@qq.com>
2016-08-09 21:15:21 +0800

    有错

---
+++ src/com/peng/pers/Exercise2_13.java
@@ -0,0 +1,18 @@
+package com.peng.pers;
+
+//import javax.swing.JOptionPane;
+
+public class Exercise2_13 {
+	public static void main(String[] args){
+		//Scanner input = new Scanner(System.in);
+		/*String input = JOptionPane.showInputDialog("Enter an amount in double, for example 11.56: ");
+		double amount = input.nextDouble();
+		
+		int remainingAmount = (int)(amount * 100);
+		
+		int numberOfOneDollars = remainingAmount / 100;
+		remainingAmount %= 100; */
+		//String input = JOptionPane.showInputDialog("");
+	}
+
+}
