f9ab4b7be901eabe25370cd79c926c28aa2b3a94
penglaozi <704365628@qq.com>
2016-08-10 16:32:21 +0800

    达内培训项目

---
+++ src/com/tarena/pers/day02pm_for.java
@@ -0,0 +1,19 @@
+package com.tarena.pers;
+
+
+public class day02pm_for {
+	public static void main(String[] args){
+		int nCount = 0;
+		double dSum = 0.0;
+		for(nCount = 0; nCount < 100; ++nCount){
+			double dTemp = 1.0 / (nCount * 2 + 1);
+			if(0 == nCount % 2)
+				dSum = dSum + dTemp;
+			else
+				dSum = dSum - dTemp;
+		}
+		double dPi = 0.0;
+		dPi = 4 * dSum;
+		System.out.println(dPi);
+	}
+}
